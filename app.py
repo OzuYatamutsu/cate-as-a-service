707b25b8cd68a989bb16b167b78444e54988aa56
Sean Collins <secollins@linkedin.com>
2017-04-10 13:25:03 -0400

    Redirect on GET

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from text import cateify
-from flask import Flask, request
+from flask import Flask, request, redirect
  
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
-def index_post():
-    if request.method == 'GET':
-        return ''
+def index_post(): 
     input_text = request.form['text']
     return cateify(input_text)
 
 @app.route('/', methods=['GET'])
 def index_get():
-    return ''
+    # (Redirects to repo)
+    redirect_location = 'https://github.com/OzuYatamutsu/cateify-as-a-service'
+    return redirect(redirect_location) 
 
 app.run()
 
