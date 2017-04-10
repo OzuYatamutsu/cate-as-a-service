c30dc8fbfe4aa5a3b6f44a0c3cbb12764533eff8
Sean Collins <secollins@linkedin.com>
2017-04-10 14:23:20 -0700

    Accept both raw param and json

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from text import cateify
 from flask import Flask, request, redirect
 from sys import argv
+from json import loads
  
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
-def index_post(): 
-    input_text = request.form['text']
+def index_post():
+    input_text = ''
+    
+    try:
+        input_text = request.get_json(force=True)
+        input_text = input_text.get('text', '')
+    except Exception as e:
+        if request.form is not None:
+            input_text = request.form.get('text', '')
     return cateify(input_text)
 
 @app.route('/', methods=['GET'])
